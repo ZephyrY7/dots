a21e485f4457819ec40c9405b19c3555aaa5b6bb
Zephyr <117508594+ZephyrY7@users.noreply.github.com>
2023-10-01 21:49:59 +0800

    Update powermenu.zsh

--- hypr/scripts/powermenu.zsh
+++ hypr/scripts/powermenu.zsh
@@ -3,11 +3,11 @@
 selection=$( echo -e " Lock\n Poweroff\n󰜉 Reboot\n Suspend\n Logout" | wofi -n --height 320 --width 300 --dmenu | awk '{print tolower($2)}' )
 
 case $selection in
-    shutdown)
-        systemctl poweroff -h now
+    poweroff)
+        poweroff
         ;;
     reboot)
-        systemctl reboot
+        reboot
         ;;
     suspend)
         sleep 0.4 && swaylock && systemctl suspend
